63a0ca3ea38a7aef7de23b7666e130cd0df1ab8c
MinHwan-Park <parkmh1110@naver.com>
2021-03-10 15:21:06 +0900

    add java

---
+++ src/oopManagementSystem/MenuManager.java
@@ -0,0 +1,15 @@
+package oopManagementSystem;
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Music");
+		System.out.println("2. Delete Music");
+		System.out.println("3. Edit Music");
+		System.out.println("4. View Music");
+		System.out.println("5. Exit");
+	
+
+	}
+
+}
