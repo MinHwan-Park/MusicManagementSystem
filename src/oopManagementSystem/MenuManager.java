76bffc258f8a98f1ee7f97f2caa9a43c549b85d4
MinHwan-Park <parkmh1110@naver.com>
2021-03-18 11:20:11 +0900

    menu test 1

--- src/oopManagementSystem/MenuManager.java
+++ src/oopManagementSystem/MenuManager.java
@@ -1,13 +1,43 @@
 package oopManagementSystem;
 
+import java.util.Scanner;
+
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add Music");
-		System.out.println("2. Delete Music");
-		System.out.println("3. Edit Music");
-		System.out.println("4. View Music");
-		System.out.println("5. Exit");
+		
+		int num = 0;
+		Scanner input = new Scanner(System.in);
+		
+		while (num != 6) {
+			System.out.println("1. Add Music");
+			System.out.println("2. Delete Music");
+			System.out.println("3. Edit Music");
+			System.out.println("4. View Music");
+			System.out.println("5. Show a Menu");
+			System.out.println("6. Exit");
+			System.out.print("Select one number between 1~6 : ");
+			num = input.nextInt();
+			switch(num) {
+				case 1:
+					System.out.print("Title of Music : ");
+					String title = input.next();
+					System.out.print("Artisit : ");
+					String artisit = input.next();
+					break;
+				case 2:
+					
+				case 3:
+					
+				case 4:
+					
+				case 5:
+					
+				case 6:	
+					break;
+			}
+			
+		}
 	}
 
 }
